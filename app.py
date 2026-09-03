366b74706bf701b754e8eb9113ad33ad57c2ce8f
nesubat <nesubat@gmail.com>
2026-09-03 14:40:58 +1000

    updated port number

--- app.py
+++ app.py
@@ -621,8 +621,8 @@ def pdf_engine():
 
 if __name__ == '__main__':
     if getattr(sys, 'frozen', False):
-        app.run(debug=False, port=5000)
+        app.run(debug=False, port=5001)
     else:
-        app.run(debug=True, port=5000)
+        app.run(debug=True, port=5001)
 
     
